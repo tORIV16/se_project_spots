0d46f3ca6e5fad4e60e629a2d4db3faf8b5395c5
Toriv <juarez.victorhugo16@gmail.com>
2026-05-23 20:22:53 -0700

    Innitial JS Cards Update

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,23 @@
+const initialCards = [{
+  name : "Val Thorens",
+  link : "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+},{
+  name : "Restaurant terrace",
+  link : "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
+},{
+  name : "An outdoor cafe",
+  link : "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
+},{
+  name : "A very long bridge, over the forest and through the trees",
+  link : "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
+},{
+  name : "Tunnel with morning light",
+  link : "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
+},{
+  name : "Mountain house",
+  link : "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
+}];
+
 const profileNameElement = document.querySelector(".profile__name");
 const profileJobElement = document.querySelector(".profile__title")
 const editProfileBtn = document.querySelector(".profile__edit-button");
@@ -76,3 +96,7 @@ function handleAddCardSubmit(evt) {
 
 profileFormElement.addEventListener("submit", handleProfileFormSubmit);
 addCardFormElement.addEventListener("submit", handleAddCardSubmit);
+
+initialCards.forEach(function(card) {
+  console.log(card.name);
+});
